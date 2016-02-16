5ad68762a34ed2f593f667c95c49fa237a75732d
cxt510 <cxt510@bham.ac.uk>
2016-02-16 17:30:06 +0000

    Added rendering of grid

--- src/warehouse/gui/Gui.java
+++ src/warehouse/gui/Gui.java
@@ -4,22 +4,32 @@ import javax.swing.JFrame;
 
 public class Gui implements Runnable {
 	public static void main(String[] args) {
-		Gui g = new Gui(null);
+		Gui g = new Gui(TestMaps.TEST_MAP1);
 		
+		g.run();
 	}
 	
-	private Map m;
+	//private Map m;
 	private JFrame frame;
 	
 	public Gui(Map m) {
-		this.m = m;
+		//this.m = m;
 		
 		frame = new JFrame("Warehouse Viewer");
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new MapComponent(m));
+		frame.setSize(450, 500);
+		frame.setVisible(true);
 	}
 
 	@Override
 	public void run() {
-		
+		while (true) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				
+			}
+		}
 	}
 }
