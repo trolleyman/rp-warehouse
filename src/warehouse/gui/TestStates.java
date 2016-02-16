e494f45704d6d90459f2a69d107aeb136a87df85
cxt510 <cxt510@bham.ac.uk>
2016-02-16 22:51:28 +0000

    Added rotation test for GUI

--- src/warehouse/gui/TestStates.java
+++ src/warehouse/gui/TestStates.java
@@ -21,7 +21,7 @@ public class TestStates {
 			new Rectangle2D.Double(2.5, 1.5, 1, 3),
 			new Rectangle2D.Double(4.5, 1.5, 1, 3),
 		}), new Robot[] {
-			new Robot("Jeff" , 0, 0, 90.0),
+			new Robot("Jeff" , 5, 5, 19.0),
 			new Robot("Nigel", 3, 0, 0.0),
 			new Robot("Dave" , 0, 3, 180.0 + 45.0),
 			new Robot("Other", 4, 2, 270.0 + 45.0),
