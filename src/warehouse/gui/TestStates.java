5ebc98609c032c3cb3237842d7ca8615f6e1330d
cxt510 <cxt510@bham.ac.uk>
2016-02-16 22:23:35 +0000

    Added arrow heads to robot

--- src/warehouse/gui/TestStates.java
+++ src/warehouse/gui/TestStates.java
@@ -11,8 +11,8 @@ public class TestStates {
 			new Rectangle2D.Double(2.5, 2.5, 1, 0.5),
 			new Rectangle2D.Double(4.5, 1.5, 2, 1),
 		}), new Robot[] {
-			new Robot("Jeff" , 0, 0, 0.0),
-			new Robot("Nigel", 3, 0, 90.0),
+			new Robot("Jeff" , 0, 0, 90.0),
+			new Robot("Nigel", 3, 0, 0.0),
 			new Robot("Dave" , 0, 3, 180.0 + 45.0),
 			new Robot("Other", 4, 2, 270.0 + 45.0),
 		});
@@ -21,8 +21,8 @@ public class TestStates {
 			new Rectangle2D.Double(2.5, 1.5, 1, 3),
 			new Rectangle2D.Double(4.5, 1.5, 1, 3),
 		}), new Robot[] {
-			new Robot("Jeff" , 0, 0, 0.0),
-			new Robot("Nigel", 3, 0, 90.0),
+			new Robot("Jeff" , 0, 0, 90.0),
+			new Robot("Nigel", 3, 0, 0.0),
 			new Robot("Dave" , 0, 3, 180.0 + 45.0),
 			new Robot("Other", 4, 2, 270.0 + 45.0),
 		});
