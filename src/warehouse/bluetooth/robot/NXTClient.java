5817488ccbd13ef91ffc510f93366d671bb945ef
Reece Bennett <reece.bennett1@gmail.com>
2016-02-18 12:17:07 +0000

    Add server side code for connections

--- src/warehouse/bluetooth/robot/NXTClient.java
+++ src/warehouse/bluetooth/robot/NXTClient.java
@@ -1,9 +1,17 @@
 package warehouse.bluetooth.robot;
 
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
+
 public class NXTClient {
 
   public static void main(String[] args) {
 
+    System.out.println("NXTClient");
+    System.out.println("Waiting for BT");
+    BTConnection connection = Bluetooth.waitForConnection();
+    System.out.println("Connected!");
+    
   }
 
 }
