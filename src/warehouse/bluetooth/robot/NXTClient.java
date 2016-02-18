8ecf8a940173df930d732d2f29405a855d7a7557
Reece Bennett <reece.bennett1@gmail.com>
2016-02-18 11:02:12 +0000

    Add Client files for NXT

---
+++ src/warehouse/bluetooth/robot/NXTClient.java
@@ -0,0 +1,9 @@
+package warehouse.bluetooth.robot;
+
+public class NXTClient {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/warehouse/bluetooth/robot/NXTReceiver.java
@@ -0,0 +1,5 @@
+package warehouse.bluetooth.robot;
+
+public class NXTReceiver extends Thread {
+
+}
