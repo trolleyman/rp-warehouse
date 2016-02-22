2ee53e2b9367488d7c9e067fc09fe9de4ee2eac8
jdp553 <jdp553@cs.bham.ac.uk>
2016-02-22 17:25:33 +0000

    Prototype A* search

---
+++ pc/src/warehouse/pc/shared/RouteFinder.java
@@ -0,0 +1,130 @@
+package warehouse.pc.shared;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class RouteFinder {
+	
+	private ArrayList<Junction> nodes;					//Stores all junction data from current map
+	private ArrayList<Junction> searched;				//Closed set, nodes already searched
+	private LinkedHashMap<Junction,Integer> frontier;	//Frontier, integer value is moves from start node
+	private HashMap<Junction,Junction> cameFrom;		//Pointers to junctions once path is found
+	
+	public RouteFinder(Map _map) {		
+		
+		nodes = new ArrayList<Junction>();
+		for (int i = 0; i < _map.getWidth(); i++) {
+			for (int j = 0; j < _map.getHeight(); j++) {
+				
+				nodes.add(_map.getJunction(i, j));
+			}
+		}
+			
+	}
+	
+	public ArrayList<Junction> findRoute(Junction start, Junction goal)
+	{
+		
+		if (!nodes.contains(start) || !nodes.contains(goal)) {	//Must be valid args
+			return null;
+		}
+		
+		searched = new ArrayList<Junction>();
+		frontier = new LinkedHashMap<Junction,Integer>();
+		cameFrom = new HashMap<Junction,Junction>();
+		frontier.put(start, 0);		//Initializes search
+		Junction currentJunct = null;
+		
+		while (!frontier.isEmpty()) {
+			
+			int minCost = -1;
+			int pathEstimate = 0;
+			int movesFromStart = 0;
+
+			//Iterate through frontier to find lowest cost junction
+			for (Entry<Junction, Integer> entry : frontier.entrySet())
+			{	
+				movesFromStart = entry.getValue();
+				pathEstimate = movesFromStart + getHeuristic(entry.getKey(), goal);
+				
+			    if (minCost < 0) {
+			    	minCost = pathEstimate;
+			    	currentJunct = entry.getKey();
+			    }
+			    
+			    else if (pathEstimate < minCost){
+			    	minCost = pathEstimate;
+			    	currentJunct = entry.getKey();
+
+			    }
+			}
+			
+			if ((currentJunct.getX() == goal.getX()) && (currentJunct.getY() == goal.getY())) {
+				return makePath(start, goal);
+			}
+			
+			frontier.remove(currentJunct);
+			searched.add(currentJunct);
+			
+			for(int dpad = 0; dpad < 3; dpad++) {
+				
+				Junction neighbour = null;
+				
+				switch(dpad) {		//Easier for iteration to find available moves
+				
+				case 0 : neighbour = currentJunct.getJunction(Direction.YPos);
+							break;
+				case 1 : neighbour = currentJunct.getJunction(Direction.YNeg);
+							break;
+				case 2 : neighbour = currentJunct.getJunction(Direction.XPos);
+							break;
+				case 3 : neighbour = currentJunct.getJunction(Direction.XNeg);
+							break;
+				}
+				
+				if ((neighbour.equals(null)) || (searched.contains(neighbour)))
+						continue;
+				
+				if (!frontier.containsKey(neighbour)){
+					frontier.put(neighbour, movesFromStart + 1); //For safety
+				}
+				
+				else if ((movesFromStart + 1) >= frontier.get(neighbour))
+					continue;
+				
+				frontier.remove(neighbour);
+				frontier.put(neighbour, movesFromStart + 1);
+				
+				cameFrom.put(neighbour, currentJunct);
+				
+			}
+						
+			
+		}
+		
+		
+		return null;		
+	}
+
+	
+	public ArrayList<Junction> makePath(Junction start, Junction current) {
+		
+		ArrayList<Junction> completePath = new ArrayList<Junction>();
+		completePath.add(current);
+		
+		while ((start.getX() != current.getX()) || (start.getY() != current.getY())) {
+			
+			completePath.add(cameFrom.get(current));
+			current = cameFrom.get(current);
+			
+		}
+		
+		return completePath;
+	}
+	
+	//Heuristic calculator in Manhattan distance
+	public int getHeuristic(Junction current, Junction goal)
+	{
+		return (Math.abs(current.getX() - goal.getX()) + Math.abs(current.getY() - goal.getY()));
+	}
+}
