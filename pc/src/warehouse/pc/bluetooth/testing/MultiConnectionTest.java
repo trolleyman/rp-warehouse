018dfb1314810957b2d987261f8fd890d0aa2adb
Reece Bennett <reece.bennett1@gmail.com>
2016-03-03 19:43:16 +0000

    Add a multi-robot connection test
    This currently fails as the bluetooth operations must be synchronised
    as they are not thread safe.

--- pc/src/warehouse/pc/bluetooth/testing/MultiConnectionTest.java
+++ pc/src/warehouse/pc/bluetooth/testing/MultiConnectionTest.java
@@ -7,15 +7,17 @@ import java.util.LinkedList;
 import lejos.pc.comm.NXTInfo;
 import warehouse.pc.bluetooth.BTServer;
 
-public class TestOpenServer implements Runnable {
+public class MultiConnectionTest implements Runnable {
 	
-	private final String name = "Dobot";
-	private final String address = "0016530FD7F4";
+	private final String name1 = "Dobot";
+	private final String address1 = "0016530FD7F4";
+	private final String name2 = "Vader";
+	private final String address2 = "0016531B5A19";
 	
 	private BTServer server;
 
 	public static void main(String[] args) {
-		// Custom System.out.println
+	// Custom System.out.println
     PrintStream stream = new PrintStream(System.out) {
       public void println(String s) {
         String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
@@ -27,22 +29,17 @@ public class TestOpenServer implements Runnable {
     };
     System.setOut(stream);
 		
-		new TestOpenServer().run();
+		new MultiConnectionTest().run();
 	}
 
 	@Override
 	public void run() {
 		server = new BTServer();
-		if (server.open(new NXTInfo(BTServer.btProtocol, name, address))) {
-			server.sendCommands(name, new LinkedList<String>(Arrays.asList("left", "right", "forward")));
-			
-			try {
-				Thread.sleep(20000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-			server.sendCommands(name, new LinkedList<String>(Arrays.asList("forward", "forward", "right", "left")));
+		if (server.open(new NXTInfo(BTServer.btProtocol, name2, address2))) {
+			//server.sendCommands(name2, new LinkedList<String>(Arrays.asList("right", "right", "forward", "left")));
+		}
+		if (server.open(new NXTInfo(BTServer.btProtocol, name1, address1))) {
+			server.sendCommands(name1, new LinkedList<String>(Arrays.asList("left", "right", "forward")));
 		}
 	}
 }
