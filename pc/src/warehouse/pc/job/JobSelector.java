0915bd1512e42d0501997fb2d0fbc488cecd9483
sxr549 <sxr549@student.bham.ac.uk>
2016-03-10 11:10:56 +0000

    Added initial TSP code

--- pc/src/warehouse/pc/job/JobSelector.java
+++ pc/src/warehouse/pc/job/JobSelector.java
@@ -1,13 +1,10 @@
 package warehouse.pc.job;
 
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Optional;
-
-import warehouse.pc.search.RouteFinder;
-import warehouse.pc.shared.Direction;
 import warehouse.pc.shared.Junction;
 import warehouse.pc.shared.Map;
+import warehouse.pc.shared.TestMaps;
 
 /**
  * Creates a queue of Jobs to be assigned
@@ -15,8 +12,8 @@ import warehouse.pc.shared.Map;
 public class JobSelector {
 	
 	private ArrayList<Job> jobs;
-	private RouteFinder routeFinder;
 	private ArrayList<Junction> dropLocations;
+	private TSPDistance tsp;
 	
 	//Currently: "locations.csv", "items.csv", "jobs.csv", "drops.csv"
 	public JobSelector(String locationsLocation, String itemsLocation, String jobsLocation, String dropsLocation, Map map){
@@ -26,7 +23,9 @@ public class JobSelector {
 		JobList jobList = new JobList(jobsLocation, itemList);
 		DropList dropList = new DropList(dropsLocation);
 		dropLocations = dropList.getList();
-		routeFinder = new RouteFinder(map);
+		//dropLocations = new ArrayList<Junction>();
+		//dropLocations.add(new Junction(3,3));
+		tsp = new TSPDistance(map, dropLocations);
 		
 		jobs = jobList.getList();
 		jobs.sort(new RewardComparator()); //This sorts the jobs into reward order
@@ -42,16 +41,18 @@ public class JobSelector {
 	public Optional<Job> getJob(int x, int y, float freeWeight){
 		
 		//Returns the first job that is supported by the robots free weight and removes it from the queue
+		/*
+		for(int i = 0; i < jobs.size(); i++){
+			if(jobs.get(i).getTotalWeight() <= freeWeight){
+				Job next = jobs.get(i);
+				jobs.remove(i);
+				return Optional.of(next);
+			}
+		}
+		return Optional.empty();
 		
-//		for(int i = 0; i < jobs.size(); i++){
-//			if(jobs.get(i).getTotalWeight() <= freeWeight){
-//				Job next = jobs.get(i);
-//				jobs.remove(i);
-//				return Optional.of(next);
-//			}
-//		}
-//		return Optional.empty();
-		
+		*/
+
 		//Filters out all Jobs that are not supported by the robot's free weight
 		ArrayList<Job> available = new ArrayList<Job>();
 		for(int i = 0; i < jobs.size(); i++){
@@ -64,7 +65,7 @@ public class JobSelector {
 			Job next = available.get(0);
 			//The Job with the highest reward per step ratio will be selected from the remaining list
 			for(int j = 1; j < available.size(); j++){
-				if((available.get(j).getTotalReward()/Distance(available.get(j), x, y) > next.getTotalReward()/Distance(next, x, y))){
+				if((available.get(j).getTotalReward()/tsp.getDistance(available.get(j), x, y) > next.getTotalReward()/tsp.getDistance(next, x, y))){
 					next = available.get(j);
 				}
 			}
@@ -73,75 +74,6 @@ public class JobSelector {
 			//The selected Job is returned
 			return Optional.of(next);
 		}
-	}
-	
-	/**
-	 * Helper method for the Job Selector that uses A* to find the minimum number of steps required by the robot to complete a certain Job
-	 * @param job The Job used in calculations
-	 * @param x The starting x-coordinate of the robot
-	 * @param y The starting y-coordinate of the robot
-	 * @return The minimum number of steps required to finish the Job
-	 */
-	private int Distance(Job job, int x, int y){
-		//Retrieves the list of items from the Job
-		ArrayList<ItemQuantity> items = job.getItems();
-		
-		int best = 0;
-		int total = 0;
-		
-		int[] numbers = new int[items.size()];
-		for(int n = 0; n < items.size(); n++) numbers[n] = n;
-		//Finds all possible orderings in which the items can be picked up
-		List<List<Integer>> permutations = permute(numbers);
-		
-		//Finds the number of steps required for every item ordering
-		for(int i = 0; i < permutations.size(); i++){
-			//Uses A* to find the distance (number of steps) between two nodes on the map
-			//Distance from start position to first item:
-			total = routeFinder.findRoute(new Junction(x, y), items.get(permutations.get(i).get(0)).getItem().getJunction(), Direction.X_POS).size();
-			for(int j = 1; j < items.size(); j++){
-				//Distance between every item after the first:
-				total += routeFinder.findRoute(items.get(permutations.get(i).get(j-1)).getItem().getJunction(), items.get(permutations.get(i).get(j)).getItem().getJunction(), Direction.X_POS).size();	 
-			}
-			//Distance between final item and the closest drop off point
-			int dropdistance = routeFinder.findRoute(items.get(permutations.get(i).get(items.size() - 1)).getItem().getJunction(), dropLocations.get(0) , Direction.X_POS).size();
-			for(int k = 1; k < dropLocations.size(); k++){
-				if (routeFinder.findRoute(items.get(permutations.get(i).get(items.size() - 1)).getItem().getJunction(), dropLocations.get(k), Direction.X_POS).size() < dropdistance){
-					dropdistance = routeFinder.findRoute(items.get(permutations.get(i).get(items.size() - 1)).getItem().getJunction(), dropLocations.get(k), Direction.X_POS).size();
-				}
-			}
-			//Total distance of current permutation
-			total += dropdistance;
-			//If the current permutation is more efficient than the best one calculated so far, then the current one is set to be the best
-			if (total < best) best = total;
-		}
-		//Returns the minimal distance (number of steps) needed to complete the Job
-		return best;
-		
-	}
-	
-	/**
-	 * Helper method to the above helper method
-	 * @param numbers An array of numbers to be ordered
-	 * @return A list of all possible permutations of the numbers in the array
-	 */
-	private List<List<Integer>> permute(int[] numbers) {
-	    List<List<Integer>> permutations = new ArrayList<List<Integer>>();
-	    permutations.add(new ArrayList<Integer>());
-
-	    for (int i = 0; i < numbers.length; i++) {
-	        List<List<Integer>> current = new ArrayList<List<Integer>>();
-	        for (List<Integer> permutation : permutations) {
-	            for (int j = 0, n = permutation.size() + 1; j < n; j++) {
-	                List<Integer> temp = new ArrayList<Integer>(permutation);
-	                temp.add(j, numbers[i]);
-	                current.add(temp);
-	            }
-	        }
-	        permutations = new ArrayList<List<Integer>>(current);
-	    }
-
-	    return permutations;
 	}
 	
 	/**
@@ -169,5 +101,21 @@ public class JobSelector {
 	 */
 	public ArrayList<Job> getList(){
 		return this.jobs;
+	}
+	
+	public static void main(String[] args){
+		JobSelector js = new JobSelector("locations.csv", "items.csv", "jobs.csv", "drops.csv", TestMaps.TEST_MAP4);
+		
+		long startTime = System.nanoTime();
+		System.out.println(js.getJob(5, 3, 100));
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime);
+		System.out.println(duration/1000000 + "ms");
+		
+		long twostartTime = System.nanoTime();
+		System.out.println(js.getJob(0, 0, 5));
+		long twoendTime = System.nanoTime();
+		long twoduration = (twoendTime - twostartTime);
+		System.out.println(twoduration/1000000 + "ms");
 	}
 }
---
+++ pc/src/warehouse/pc/job/TSPDistance.java
@@ -0,0 +1,116 @@
+package warehouse.pc.job;
+
+import java.util.ArrayList;
+
+import warehouse.pc.search.RouteFinder;
+import warehouse.pc.shared.Direction;
+import warehouse.pc.shared.Junction;
+import warehouse.pc.shared.Map;
+
+public class TSPDistance {
+	
+	private RouteFinder routeFinder;
+	ArrayList<Junction> dropLocations;
+	
+	public TSPDistance(Map map, ArrayList<Junction> dropLocations){
+		routeFinder = new RouteFinder(map);
+		this.dropLocations = dropLocations;
+	}
+	
+	public int getDistance(Job job, int x, int y){
+		ArrayList<ItemQuantity> items = job.getItems();
+		int[] dToItem = new int[items.size()];
+		for(int i=0; i<items.size(); i++){
+			dToItem[i] = (routeFinder.findRoute(new Junction(x, y), items.get(i).getItem().getJunction(), Direction.X_POS).size());
+		}
+		ArrayList<ItemQuantity> res = new ArrayList<ItemQuantity>();
+		
+		int first = 0;
+		int firstValue = Integer.MAX_VALUE;
+		for(int i=0; i<items.size(); i++){
+			if(dToItem[i] < firstValue){
+				first = i;
+				firstValue = dToItem[i];
+			}
+		}
+		ItemQuantity firstSelected = items.get(first);
+		int totalDistance = firstValue;
+		res.add(firstSelected);
+		items.remove(firstSelected);
+
+		Object[] itemArray = items.toArray();
+		int[][] itemToItem = new int[items.size()][items.size()];
+		for(int i=0; i<items.size(); i++){
+			for(int j=0; j<items.size(); j++){
+				itemToItem[i][j] = (routeFinder.findRoute(items.get(i).getItem().getJunction(), items.get(j).getItem().getJunction(), Direction.X_POS).size());
+			}
+		}
+		int[] itemToDrop = new int[items.size()];
+		for(int i=0; i<items.size(); i++){
+			int temp = Integer.MAX_VALUE;
+			for(int j=0; j<dropLocations.size(); j++){
+				if(routeFinder.findRoute(items.get(i).getItem().getJunction(), dropLocations.get(j).getJunction(Direction.X_POS), Direction.X_POS).size() < temp){
+					temp = routeFinder.findRoute(items.get(i).getItem().getJunction(), dropLocations.get(j).getJunction(Direction.X_POS), Direction.X_POS).size();
+				}
+			}
+			itemToDrop[i] = temp;
+		}
+		
+		while(!items.isEmpty()){
+			int next = Integer.MAX_VALUE;
+			int nextIndex = 0;
+			int resIndex = 0;
+			for(int i=0; i<items.size(); i++){
+				int firstItem = 0;
+				int firstRes = 0;
+				int secondRes = 0;
+				for(int k=0; k<itemArray.length; k++){
+					if(itemArray[k].equals(items.get(i))){
+						firstItem = k;
+						break;
+					}
+				}
+				for(int l=0; l<itemArray.length; l++){
+					if(itemArray[l].equals(res.get(0))){
+						firstRes = l;
+						break;
+					}
+				}
+				for(int m=0; m<itemArray.length; m++){
+					if(itemArray[m].equals(res.get(res.size()-1))){
+						secondRes = m;
+						break;
+					}
+				}
+				int distance1 = itemToItem[firstItem][firstRes];
+				int distance2 = itemToItem[firstItem][secondRes];
+				if(distance1 < distance2 && distance1 < next){
+					next = distance1;
+					nextIndex = i;
+					resIndex = 0;
+				}
+				else if (distance2 < distance1 && distance2 < next){
+					next = distance2;
+					nextIndex = i;
+					resIndex = res.size()-1;
+				}
+			}
+			
+			ItemQuantity nextSelected = items.get(nextIndex);
+			totalDistance += next;
+			res.add(resIndex, nextSelected);;
+			items.remove(nextSelected);	
+		}
+		
+		int lastValue = Integer.MAX_VALUE;
+		for(int i=0; i<res.size(); i++){
+			if(itemToDrop[i] < lastValue) {
+				lastValue = itemToDrop[i];
+			}
+		}
+		
+		totalDistance += lastValue;
+		
+		return totalDistance;		
+	}
+}
