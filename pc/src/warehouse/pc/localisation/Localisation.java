1007ec6ef0bdb792f34c21299ae7cc43e7d2256d
Callum Tolley <cxt510@bham.ac.uk>
2016-03-17 17:18:10 +0000

    Added more stuff, not finished

--- pc/src/warehouse/pc/localisation/Localisation.java
+++ pc/src/warehouse/pc/localisation/Localisation.java
@@ -4,26 +4,46 @@ import warehouse.pc.shared.Direction;
 import warehouse.pc.shared.Map;
 
 public class Localisation {
+	private static final double THRESHOLD = 0.9;
 	private Map map;
-	private Float[][] probabilities;
+	private Double[][] probabilities;
+	private int numValidLocations;
 	
+	// We don't know the starting location
 	public Localisation(Map _map) {
 		this.map = _map;
 		this.probabilities = new Float[map.getHeight()][map.getWidth()];
+		numValidLocations = 0;
 		for (int y = 0; y < map.getHeight(); y++) {
 			for (int x = 0; x < map.getWidth(); x++) {
-				
+				if (map.getJunction(x, y) != null) {
+					numValidLocations += 1;
+				}
+			}
+		}
+		
+		double init = 1.0 / numValidLocations;
+		for (int y = 0; y < map.getHeight(); y++) {
+			for (int x = 0; x < map.getWidth(); x++) {
+				if (map.getJunction(x, y) != null) {
+					probabilities[y][x] = init;
+				}
 			}
 		}
 	}
 	
 	public void distanceRecieved(Direction dir, int _dist) {
 		// Update probabilities at a junction
-		
+		for (int y = 0; y < map.getHeight(); y++) {
+			for (int x = 0; x < map.getWidth(); x++) {
+				// Get probability for x, y
+				map.getProbability(x, y, dir, _dist);
+			}
+		}
 	}
 	
 	public boolean isFinished() {
-		
+		// If one location is above THRESHOLD
 	}
 	
 	public int getLikelyX() {
