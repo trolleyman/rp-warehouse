0c1ba5c8bc6278fb5bd983a384eeb75832db7b00
Denis Makula <dxm581@student.bham.ac.uk>
2016-03-18 02:34:33 +0000

    Modified Reece's dynamic rotation technique and added it in Junction Behaciour, fixed Waiting Behaviour ( not tested! ).

--- nxt/src/warehouse/nxt/motion/behaviours/JunctionBehaviour.java
+++ nxt/src/warehouse/nxt/motion/behaviours/JunctionBehaviour.java
@@ -5,6 +5,7 @@ import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.subsumption.Behavior;
 import warehouse.nxt.motion.LightSensorCalibration;
 import warehouse.nxt.motion.PathProvider;
+import warehouse.nxt.utils.Robot;
 import warehouse.shared.Constants;
 
 
@@ -18,15 +19,18 @@ public class JunctionBehaviour implements Behavior {
 	private PathProvider provider;
 	private volatile boolean shouldTakeControl;
 	
-	public JunctionBehaviour( DifferentialPilot _pilot, LightSensorCalibration _calibration, PathProvider _provider) {
+	private Robot myself;
+	
+	public JunctionBehaviour( DifferentialPilot _pilot, LightSensorCalibration _calibration, PathProvider _provider, Robot _robot) {
 		this.pilot = _pilot;
 		this.calibration = _calibration;
 		this.provider = _provider;
 		this.shouldTakeControl = false;
+		this.myself = _robot;
 	}
 	
 	private boolean leftOnLine() {
-		return this.calibration.leftSensor .readValue() < THRESHOLD;
+		return this.calibration.leftSensor.readValue() < THRESHOLD;
 	}
 	
 	private boolean rightOnLine() {
@@ -37,7 +41,7 @@ public class JunctionBehaviour implements Behavior {
 	public boolean takeControl() {
 		if( this.shouldTakeControl ) { return true; }
 		
-		if( this.leftOnLine() && this.rightOnLine() ) {
+		if( this.leftOnLine() && this.rightOnLine() && !this.provider.isFinished() ) {
 			Sound.beep();
 			this.shouldTakeControl = true;
 		}
@@ -48,6 +52,8 @@ public class JunctionBehaviour implements Behavior {
 	@Override
 	public void action() {
 		String direction = this.provider.getNextDirection();
+		this.myself.status = "Moving " + direction;
+		
 		pilot.setTravelSpeed(Constants.ROBOT_SPEED);
 		pilot.setRotateSpeed(Constants.ROBOT_ROTATION_SPEED);
 		//System.out.println("Jct:" + direction);
@@ -56,9 +62,9 @@ public class JunctionBehaviour implements Behavior {
 			return;
 		
 		switch( direction ) {
-			case "Backward": pilot.rotate( 180 ); break;
-			case "Left"    : pilot.travel( TURNING_OFFSET ); pilot.rotate( 90 ); break;
-			case "Right"   : pilot.travel( TURNING_OFFSET ); pilot.rotate( -90 ); break;
+			case "Backward": this.rotate( "back" ); break;
+			case "Left"    : pilot.travel( TURNING_OFFSET ); this.rotate( "left" ); break;
+			case "Right"   : pilot.travel( TURNING_OFFSET ); this.rotate( "right" ); break;
 			case "Forward" : pilot.travel( TURNING_OFFSET ); break;
 			default        : return;
 		}
@@ -69,4 +75,37 @@ public class JunctionBehaviour implements Behavior {
 	@Override
 	public void suppress() {  }
 		
+	private void rotate( String _direction ) {
+		
+		boolean right = false, left = false, back = false, rotating = false, decreased = false;
+		int counter = 0;
+		
+		switch( _direction ) {
+			case "left" : left = true; right  = false; back = false; counter = 1; break;
+			case "right" : left = false; right = true; back = false; counter = 1; break;
+			case "back" : left = false; right = false; back = true; counter = 2; break;
+			default : break;
+		}
+		
+		while( right && !this.rightOnLine() ) {
+			if( !rotating ) { this.pilot.rotateRight(); rotating = true; }
+		}
+		
+		while( left && !this.leftOnLine() ) {
+			if( !rotating ) { this.pilot.rotateLeft(); rotating = true; }
+		}
+		
+		while( back && counter > 0 ) {
+			while( this.leftOnLine() ) { if( !decreased ) { counter--; decreased = true; } }
+			
+			decreased = false;
+			
+			if( !rotating ) { this.pilot.rotateLeft(); rotating = true; }
+		}
+		
+		this.pilot.stop();
+		rotating = false;
+		
+	}
+
 }
--- nxt/src/warehouse/nxt/motion/behaviours/WaitingBehaviour.java
+++ nxt/src/warehouse/nxt/motion/behaviours/WaitingBehaviour.java
@@ -1,27 +1,42 @@
 package warehouse.nxt.motion.behaviours;
 
 import lejos.robotics.subsumption.Behavior;
+import warehouse.nxt.motion.LightSensorCalibration;
 import warehouse.nxt.motion.PathProvider;
 import warehouse.nxt.utils.Robot;
 
 
 
 public class WaitingBehaviour implements Behavior {
+
+	private static final double THRESHOLD = 62.0;
+
 	private PathProvider provider;
 	private volatile boolean shouldTakeControl;
 	private Robot myself;
 	
-	public WaitingBehaviour( PathProvider _provider, Robot _robot ) {
+	private LightSensorCalibration calibration;
+	
+	public WaitingBehaviour( LightSensorCalibration _calibration, PathProvider _provider, Robot _robot ) {
 		this.provider = _provider;
 		this.shouldTakeControl = false;
 		this.myself = _robot;
+		this.calibration = _calibration;
 	}
 		
+	private boolean leftOnLine() {
+		return this.calibration.leftSensor.readValue() < THRESHOLD;
+	}
+	
+	private boolean rightOnLine() {
+		return this.calibration.rightSensor.readValue() < THRESHOLD;
+	}
+
 	@Override
 	public boolean takeControl() {
 		if( this.shouldTakeControl ) { return true; }
 		
-		if( this.provider.isFinished() ) {
+		if( this.leftOnLine() && this.rightOnLine() && this.provider.isFinished() ) {
 			if( !this.myself.ready ) { this.myself.ready = true; this.myself.status = "Idle"; }
 			this.shouldTakeControl = true;
 		}
